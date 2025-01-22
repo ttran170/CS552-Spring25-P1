0b1083c9dde070053fe3aa32960988fae74137b3
Tyler Tran <tjtranwok@gmail.com>
2025-01-22 15:03:34 -0800

    Update lab.h

--- src/lab.h
+++ src/lab.h
@@ -1,1 +1,22 @@
-/**Update this file with the starter code**/
+#ifndef LAB_H
+#define LAB_H
+#include <stdlib.h>
+#include <stdbool.h>
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+/**
+* @brief A node in the list
+*
+*/
+typedef struct node{
+  void *data;
+  struct node *next;
+  struct node *prev;
+} node_t;
+
+#ifdef __cplusplus
+}
+#endif
+#endif
