bf8fee6ccf3bbec7cfd012608dff3917f704998c
Tyler Tran <tjtranwok@gmail.com>
2025-01-31 15:06:25 -0800

    Fixes for code review

--- src/lab.c
+++ src/lab.c
@@ -1,16 +1,9 @@
 #include "lab.h"
 
 /**
-* @brief Create a new list with callbacks that know how to deal with the data
-that
-* list is storing. The caller must pass the list to list_destroy when finished
-to
-* free any memory that was allocated.
-*
-* @param destroy_data Function that will free the memory for user supplied data
-* @param compare_to Function that will compare two user data elements
-* @return struct list* pointer to the newly allocated list.
+* Added return statement that I forgot
 */
+
 list_t *list_init(void (*destroy_data)(void *),
 int (*compare_to)(const void *, const void *)){
     list_t* out = (list_t*) malloc(sizeof(list_t));
@@ -23,14 +16,9 @@ int (*compare_to)(const void *, const void *)){
     head->next = head;
     head->prev = head;
     out->head = head;
+    return out;
 }
-/**
-* @brief Destroy the list and and all associated data. This functions will
-call
-* destroy_data on each nodes data element.
-*
-* @param list a pointer to the list that needs to be destroyed
-*/
+
 void list_destroy(list_t **list){
     if (list == NULL){
         return;
@@ -41,7 +29,7 @@ void list_destroy(list_t **list){
 
     /**
      * loop starts at first element of the list
-     * then increments size of list, so sentinel node will be freed
+     * then increments size of list, so sentinel node will be freed last
      */
     for (int i = 0; i < target->size ; i++)
     {
@@ -60,12 +48,9 @@ void list_destroy(list_t **list){
     free(target);
     
 }
+
 /**
-* Adds data to the front of the list
-*
-* @param list a pointer to an existing list.
-* @param data the data to add
-* @return A pointer to the list
+* Added return statement that I forgot
 */
 list_t *list_add(list_t *list, void *data){
     if (list == NULL){
@@ -78,16 +63,10 @@ list_t *list_add(list_t *list, void *data){
     new_node->prev = list->head;
     list->head->next = new_node;
     sec_node->prev = new_node;
+    return list;
 
 }
-/**
-* @brief Removes the data at the specified index. If index is invalid
-* then this function does nothing and returns NULL
-*
-* @param list The list to remove the element from
-* @param index The index
-* @return void* The data that was removed or NULL if nothing was removed
-*/
+
 void *list_remove_index(list_t *list, size_t index){
     if (list == NULL){
         return NULL;
@@ -108,16 +87,7 @@ void *list_remove_index(list_t *list, size_t index){
     }
     return out;
 }
-/**
-* @brief Search for any occurrence of data from the list.
-* Internally this function will call compare_to on each item in the list
-* until a match is found or the end of the list is reached. If there are
-* multiple copies of the same data in the list the first one will be returned.
-*
-* @param list the list to search for data
-* @param data the data to look for
-* @return The index of the item if found or -1 if not
-*/
+
 int list_indexof(list_t *list, void *data){
     if (list == NULL){
         return -1;
